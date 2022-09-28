3ca281449d9a965fcf426225bbc457d5f5108b1f
Kyrylo Yakymenko <kya@combination.se>
2022-09-29 01:37:21 +0200

    Switch to Code-first and add a delay to the comments resolver

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 using hot_defer.queries;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services
     .AddGraphQLServer()
-    .AddQueryType<Query>().AddDirectiveType<DeferDirectiveType>().AddDirectiveType<StreamDirectiveType>();
+    .AddQueryType<QueryType>().AddDirectiveType<DeferDirectiveType>().AddDirectiveType<StreamDirectiveType>();
 
 var app = builder.Build();
 
--- queries/books.cs
+++ queries/books.cs
@@ -1,138 +1,167 @@
 using hot_defer.entities;
 
-namespace hot_defer.queries
+namespace hot_defer.queries;
+
+public class Query
 {
-    public class Query
+    private readonly Book book = new()
     {
+        Title = "C# in depth.",
 
-        Book book = new Book
+        Comments = new[]
         {
-            Title = "C# in depth.",
-
-            Comments = new Comment[]
-                    {
-                        new Comment
-                        {
-
-                            Body = "This is a comment 1"
-                        },
-                         new Comment
-                        {
-
-                            Body = "This is a comment 2"
-                        }
-                    }
-        };
-
-        IEnumerable<Book> books = new Book[]{
-            new Book {
-                Id=1,
-            Title = "C# in depth.",
-            Comments = new Comment[]
-                    {
-                        new Comment
-                        {
-                            Id=1,
-                            Body = "This is a comment 1"
-                        },
-                         new Comment
-                        {
-                            Id=3,
-                            Body = "This is a comment 2"
-                        },
-                         new Comment
-                        {
-                            Id=4,
-                            Body = "This is a comment 3"
-                        },
-                         new Comment
-                        {
-                            Id=5,
-                            Body = "This is a comment 4"
-                        },
-                         new Comment
-                        {
-                            Id=6,
-                            Body = "This is a comment 5"
-                        },
-                         new Comment
-                        {
-                            Id=7,
-                            Body = "This is a comment 6"
-                        },
-                         new Comment
-                        {
-                            Id=8,
-                            Body = "This is a comment 7"
-                        },
-                         new Comment
-                        {
-                            Id=9,
-                            Body = "This is a comment 8"
-                        }
-                    }
+            new()
+            {
+                Body = "This is a comment 1"
             },
-            new Book {
-                Id=2,
-            Title = "C# in depth.",
-            Comments = new Comment[]
-                    {
-                        new Comment
-                        {
-                            Id=10,
-                            Body = "This is a comment 1"
-                        },
-                         new Comment
-                        {
-                            Id=11,
-                            Body = "This is a comment 2"
-                        }
-                    }
-            },
-            new Book {
-                Id=3,
-            Title = "F# in depth.",
-            Comments = new Comment[]
-                    {
-                        new Comment
-                        {
-                            Id=12,
-
-                            Body = "This is a comment 1"
-                        },
-                         new Comment
-                        {
-                            Id=13,
-                            Body = "This is a comment 2"
-                        }
-                    }
-            },
-            new Book {
-                Id=4,
-            Title = "G# in depth.",
-            Comments = new Comment[]
-                    {
-                        new Comment
-                        {
-                            Id=14,
-
-                            Body = "This is a comment 1"
-                        },
-                         new Comment
-                        {
-                            Id=15,
-
-                            Body = "This is a comment 2"
-                        }
-                    }
+            new Comment
+            {
+                Body = "This is a comment 2"
             }
-        };
+        }
+    };
 
-        public Book GetBook() =>
-           book;
+    private readonly IEnumerable<Book> books = new[]
+    {
+        new Book
+        {
+            Id = 1,
+            Title = "C# in depth.",
+            Comments = new[]
+            {
+                new()
+                {
+                    Id = 1,
+                    Body = "This is a comment 1"
+                },
+                new Comment
+                {
+                    Id = 3,
+                    Body = "This is a comment 2"
+                },
+                new Comment
+                {
+                    Id = 4,
+                    Body = "This is a comment 3"
+                },
+                new Comment
+                {
+                    Id = 5,
+                    Body = "This is a comment 4"
+                },
+                new Comment
+                {
+                    Id = 6,
+                    Body = "This is a comment 5"
+                },
+                new Comment
+                {
+                    Id = 7,
+                    Body = "This is a comment 6"
+                },
+                new Comment
+                {
+                    Id = 8,
+                    Body = "This is a comment 7"
+                },
+                new Comment
+                {
+                    Id = 9,
+                    Body = "This is a comment 8"
+                }
+            }
+        },
+        new Book
+        {
+            Id = 2,
+            Title = "C# in depth.",
+            Comments = new[]
+            {
+                new Comment
+                {
+                    Id = 10,
+                    Body = "This is a comment 1"
+                },
+                new Comment
+                {
+                    Id = 11,
+                    Body = "This is a comment 2"
+                }
+            }
+        },
+        new Book
+        {
+            Id = 3,
+            Title = "F# in depth.",
+            Comments = new[]
+            {
+                new Comment
+                {
+                    Id = 12,
 
-        public IEnumerable<Book> GetBooks() =>
-            books;
+                    Body = "This is a comment 1"
+                },
+                new Comment
+                {
+                    Id = 13,
+                    Body = "This is a comment 2"
+                }
+            }
+        },
+        new Book
+        {
+            Id = 4,
+            Title = "G# in depth.",
+            Comments = new[]
+            {
+                new Comment
+                {
+                    Id = 14,
+
+                    Body = "This is a comment 1"
+                },
+                new Comment
+                {
+                    Id = 15,
+
+                    Body = "This is a comment 2"
+                }
+            }
+        }
+    };
+
+    public Book GetBook()
+    {
+        return book;
     }
 
+    public IEnumerable<Book> GetBooks()
+    {
+        return books;
+    }
+}
+
+public class BookType : ObjectType<Book>
+{
+    protected override void Configure(IObjectTypeDescriptor<Book> descriptor)
+    {
+        descriptor.Field(f => f.Title).Type<StringType>();
+        descriptor.Field(f => f.Comments).Resolve(async () =>
+        {
+            await Task.Delay(3000);
+            return new []
+            {
+                new Comment {Id = 11, Body = "Great book about C#!"},
+                new Comment {Id = 12, Body = "Haven't read it..."}
+            };
+        });
+    }
+}
+
+public class QueryType : ObjectType<Query>
+{
+    protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+    {
+        descriptor.Field(f => f.GetBook()).Type<BookType>();
+    }
 }
